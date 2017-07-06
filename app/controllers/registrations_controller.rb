6c3db97e585a9238a2437745b1f45eef226f9051
Sophie Figgis <sophiefiggis@gmail.com>
2017-07-06 11:53:08 +0100

    modified provider registration to have 2 step process

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,26 +2,17 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
     if params[:commit] == "Wait, I'm a Provider"
-      @provider = Provider.new(buyer_params)
+      @provider = Provider.new(sign_up_params)
       self.resource = @provider
       render :new_provider
     else
-      binding.pry
       super
     end
   end
 
-  def resource_name
-    params[:provider] ? :provider : :buyer
-  end
-
-  def resource_class
-    params[:provider] ? Provider : Buyer
-  end
-
-  private
-  def buyer_params
-    params.require(:buyer).permit(:email, :password, :password_confirmation, :type)
+  def devise_parameter_sanitizer
+    resource_name = params[:provider] ? :provider : :buyer
+    @devise_parameter_sanitizer ||= Devise::ParameterSanitizer.new(resource_class, resource_name, params)
   end
 
 end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,3 @@
 class Provider < Buyer
-  
+  validates :organisation_name, presence: true
 end
